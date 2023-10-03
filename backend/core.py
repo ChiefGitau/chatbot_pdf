178e36ff74ba93f7cc2a124610619d168477e5bd
Hidde Makimei <hidde.n.g.makimei@gmail.com>
2023-10-03 10:27:50 +0200

    adding memory and fixing issues with dependancy

--- backend/core.py
+++ backend/core.py
@@ -1,10 +1,11 @@
 import os
-from typing import Any
+from typing import Any, List, Dict
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.vectorstores import Pinecone
+from langchain.chains import  ConversationalRetrievalChain
 import pinecone
 
 from const import INDEX_NAME
@@ -12,19 +13,20 @@ from const import INDEX_NAME
 pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment="gcp-starter")
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
-    chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+    chat = ChatOpenAI(
+        verbose=True,
+        temperature=0,
     )
-    return qa({"query": query})
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
